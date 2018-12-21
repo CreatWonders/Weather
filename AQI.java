46ed1d74ebb74e6c02eddee0ea57766794fc0925
CreatWonders <33147637+CreatWonders@users.noreply.github.com>
2018-12-21 14:19:10 +0800

    Add files via upload

---
+++ AQI.java
@@ -0,0 +1,15 @@
+package com.coolweather.android.gson;
+/*空气质量的简称*/
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
